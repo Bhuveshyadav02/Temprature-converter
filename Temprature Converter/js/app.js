e1e078fbb76575a7d748af80bce125089b58e4d2
Bhuveshyadav02 <142390249+Bhuveshyadav02@users.noreply.github.com>
2023-09-07 18:20:24 +0530

    Add files via upload

---
+++ Temprature Converter/js/app.js
@@ -0,0 +1,53 @@
+// script.js
+const celsiusField = document.querySelector("#celsius");
+const degree = document.querySelector("#degree");
+const convertBtn = document.querySelector("#convert-btn");
+const tempType = document.querySelector("#temp-type");
+
+window.addEventListener("load", () => {
+  degree.value = "";
+  celsiusField.innerHTML = "";
+});
+
+if(degree.value === ""){
+  convertBtn.setAttribute("disabled","");
+  setTimeout(() => {
+    convertBtn.removeAttribute('disabled');
+  }, 4000);
+}
+
+
+convertBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  convertToCelsius();
+  convertBtn.innerHTML = "<span class='icon'><i class='fa fa-spinner fa-spin'></i> Converting...</span>";
+  setTimeout(() => {
+    convertBtn.innerHTML ="<span>Convert</span>"
+  }, 1000);
+});
+
+function convertToCelsius() {
+  let inputValue = degree.value;
+  
+  setTimeout( () => {
+    if (tempType.value === "fahrenheit") {
+      const FahrenheitToCelsius = (inputValue - 32) * (5 / 9);
+      celsiusField.innerHTML = `${FahrenheitToCelsius.toFixed(3)} &deg;c`;
+    } else if (tempType.value === "kelvin") {
+      const KelvinToCelsius = inputValue - 273.15;
+      celsiusField.innerHTML = `${KelvinToCelsius.toFixed(3)} &deg;c`;
+    }
+  }, 1200)
+}
+const timeElement = document.querySelector(".time"); // Assuming you have a <p> element with class "time"
+const currentDate = new Date();
+const Day = currentDate.getDate();
+const Month = currentDate.getMonth() + 1; // Adding 1 because getMonth() returns zero-based months (0-11)
+const Year = currentDate.getFullYear();
+
+// Create a formatted date string
+const formattedDate = `${Month}/${Day}/${Year}`;
+
+// Set the innerHTML of the <p> element to the formatted date
+timeElement.innerHTML = formattedDate;
+
